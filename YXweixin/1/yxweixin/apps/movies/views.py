a0df6f3359dc6e33e064106a0d78685424493c99
vaneoooo <vaneoooo@gmail.com>
2013-03-08 05:12:38 +0800

    add something

--- YXweixin/1/yxweixin/apps/movies/views.py
+++ YXweixin/1/yxweixin/apps/movies/views.py
@@ -29,6 +29,8 @@ def test():
     if mc.get('movie_list'):
         return mc.get('movie_list')
     content = moviespider()
+    if not content[0]:
+        return (u'哎哟~今天电影院还没发布电影信息~稍等会再来吧。')
     s = ''
     for i in range(0,len(content[0])):
         s+=''.join(content[0][i])
@@ -80,8 +82,3 @@ def index():
         return echostr
     mc.set('is_test',time.ctime(),time=50)
     return mc.get('is_test')
-    
-    
-
-
-
